22dead7cde1281761912b45ad71d7404f90bb356
shahalam3041 <shahalam3041@gmail.com>
2018-07-13 00:45:15 +0600

    Added Sliding tab in Bus routine activity

--- app/src/main/java/com/example/shahalam/navigationandfragment/BusRoutineActivity.java
+++ app/src/main/java/com/example/shahalam/navigationandfragment/BusRoutineActivity.java
@@ -1,13 +1,25 @@
 package com.example.shahalam.navigationandfragment;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BusRoutineActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_routine);
+
+        viewPager = findViewById(R.id.viewPagerID);
+        tabLayout = findViewById(R.id.tabLayoutID);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
